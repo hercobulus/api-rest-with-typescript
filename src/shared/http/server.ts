520f240566e86b555172a8fe25f5f8b86668c020
Daniel Miranda <daniel.sousa@trinto.com.br>
2022-04-14 15:07:50 -0300

    create server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,14 @@
-console.log('OLá mundo');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started on port! 3333');
+});
