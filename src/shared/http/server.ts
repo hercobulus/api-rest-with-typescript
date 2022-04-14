7956c7d853f6ae48ac814584b44a5da6ddc84c2c
Daniel Miranda <daniel.sousa@trinto.com.br>
2022-04-14 15:18:29 -0300

    create a error handler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,27 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'Error',
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'Error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
