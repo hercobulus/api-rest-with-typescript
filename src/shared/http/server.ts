85d7e9e81097cc4e4fe26e7a657ee0244630022c
Daniel Miranda <daniel.sousa@trinto.com.br>
2022-04-15 11:38:23 -0300

    validation fields with celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,13 +6,14 @@ import routes from '@shared/http/routes';
 import '@shared/typeorm';
 import logger from '@shared/logger';
 import errorHandler from './middlewares/errorHandler';
+import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(3333, () => {
