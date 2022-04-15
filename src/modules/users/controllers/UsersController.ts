c68e0fd8d11f3b669542a27c4c9f239194021328
Daniel Miranda <daniel.sousa@trinto.com.br>
2022-04-15 12:42:12 -0300

    create user routes

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -2,10 +2,10 @@ import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUserService';
 
-export default class UsersRepository {
+export default class UsersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listUsers = new ListUserService();
-    const users = listUsers.execute();
+    const users = await listUsers.execute();
 
     return response.json(users);
   }
