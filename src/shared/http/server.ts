420fdc657170c81d36cb510c3ed76c546e26ef9e
Daniel Miranda <daniel.sousa@trinto.com.br>
2022-04-14 22:41:03 -0300

    create first migration and refactoring in typeorm config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,35 +3,29 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import routes from '@shared/http/routes';
 import AppError from '@shared/errors/AppError';
-import AppDataSource from '@shared/typeorm';
+import '@shared/typeorm';
 import logger from '@shared/logger';
 
-AppDataSource.initialize()
-  .then(function () {
-    const app = express();
+const app = express();
 
-    app.use(cors());
-    app.use(express.json());
-    app.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-    app.use((error: Error, request: Request, response: Response) => {
-      if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-          status: 'Error',
-          message: error.message,
-        });
-      }
-
-      return response.status(500).json({
-        status: 'Error',
-        message: 'Internal server error',
-      });
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'Error',
+      message: error.message,
     });
+  }
 
-    app.listen(3333, () => {
-      logger('Server started on port! 3333');
-    });
-  })
-  .catch(function (e) {
-    logger(e);
+  return response.status(500).json({
+    status: 'Error',
+    message: 'Internal server error',
   });
+});
+
+app.listen(3333, () => {
+  logger('Server started on port! 3333');
+});
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,12 +1,3 @@
-import { DataSource } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'apivendas',
-});
-
-export default AppDataSource;
+createConnection();
